932c7ac8561afb9eb1b01f954da3e8206ef13eb4
Khoa Pham <onmyway133@gmail.com>
2017-05-20 18:24:44 +0200

    Add Handler

---
+++ Sources/Handler.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+/// Managed by ThemeUser to handle theme change notification
+class Handler {
+  var observer: NSObjectProtocol!
+
+  weak var host: ThemeUser?
+  var mapping = [String: (ThemeUser, Theme) -> Void]()
+
+  init(host: ThemeUser) {
+    self.host = host
+  }
+
+  func observe() {
+    observer = NotificationCenter.default.addObserver(
+      forName: Manager.themeChangeNotification,
+      object: Manager.self,
+      queue: OperationQueue.main,
+      using: { [weak self] _ in
+        self?.handle()
+    })
+  }
+
+  func handle() {
+    guard let host = host else {
+      return
+    }
+
+    guard let theme = Manager.theme else {
+      return
+    }
+
+    guard let action = mapping[String(describing: type(of: theme))] else {
+      return
+    }
+
+    action(host, theme)
+  }
+
+  deinit {
+    NotificationCenter.default.removeObserver(observer)
+  }
+}
+
