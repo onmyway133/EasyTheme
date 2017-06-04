39c2d11a6ed37f2695ca4d02f7c5582f80d9da7b
Khoa Pham <onmyway133@gmail.com>
2017-06-04 18:39:40 +0200

    Load items

--- Example/ThemesDemo/ThemesDemo/Sources/ViewController.swift
+++ Example/ThemesDemo/ThemesDemo/Sources/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Themes
+import Imaginary
 
 class ListController: UITableViewController {
 
@@ -8,12 +9,29 @@ class ListController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = UIColor.white
+
+    tableView.separatorStyle = .none
   }
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
 
     tableView.reloadData()
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+    let item = items[indexPath.row]
+
+    cell.myImageView.setImage(url: URL(string: item.imageUrl)!)
+    cell.myTitleLabel.text = item.title
+    cell.mySubtitleLabel.text = item.subtitle
+
+    return cell
   }
 }
 
