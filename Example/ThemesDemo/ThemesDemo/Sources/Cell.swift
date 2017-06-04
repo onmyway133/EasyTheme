be3ea492d1426d8587281d88847e84c1db745f19
Khoa Pham <onmyway133@gmail.com>
2017-06-04 18:48:04 +0200

    Adjust cell

--- Example/ThemesDemo/ThemesDemo/Sources/Cell.swift
+++ Example/ThemesDemo/ThemesDemo/Sources/Cell.swift
@@ -1,9 +1,21 @@
 import UIKit
+import Hue
 
 class Cell: UITableViewCell {
+  @IBOutlet weak var container: UIView!
   @IBOutlet weak var myImageView: UIImageView!
   @IBOutlet weak var myTitleLabel: UILabel!
   @IBOutlet weak var mySubtitleLabel: UILabel!
 
-  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    myImageView.layer.cornerRadius = 5
+
+    container.layer.cornerRadius = 5
+    container.backgroundColor = UIColor(hex: "#27ae60")
+    container.layer.shadowColor = UIColor.black.cgColor
+    container.layer.shadowOpacity = 0
+    container.layer.shadowOffset = CGSize(width: 1, height: 1)
+  }
 }
--- Example/ThemesDemo/ThemesDemo/Sources/ViewController.swift
+++ Example/ThemesDemo/ThemesDemo/Sources/ViewController.swift
@@ -33,5 +33,9 @@ class ListController: UITableViewController {
 
     return cell
   }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
 
