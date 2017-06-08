1f721e5e71ad0fa9c81af1b91ed31d8ac67d3a46
Khoa Pham <onmyway133@gmail.com>
2017-06-08 20:21:13 +0200

    Change theme

--- Example/YahooWather/YahooWather/ViewController.swift
+++ Example/YahooWather/YahooWather/ViewController.swift
@@ -1,18 +1,56 @@
-//
-//  ViewController.swift
-//  YahooWather
-//
-//  Created by Khoa Pham on 08/06/2017.
-//  Copyright © 2017 Fantageek. All rights reserved.
-//
-
 import UIKit
+import Themes
+import DayNightSwitch
+
+struct MyTheme: Theme {
+  let image: UIImage
+  let textColor: UIColor
+
+  init(image: UIImage, textColor: UIColor) {
+    self.image = image
+    self.textColor = textColor
+  }
+}
+
+struct ThemeList {
+  static let day = MyTheme(image: UIImage(named: "day")!, textColor: UIColor.darkGray)
+  static let night = MyTheme(image: UIImage(named: "night")!, textColor: UIColor.white)
+}
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var degreeLabel: UILabel!
+  @IBOutlet weak var weatherLabel: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    Manager.theme = ThemeList.day
+
+    let dayNightSwitch = DayNightSwitch(frame: CGRect(x: view.bounds.size.width - 120,
+                                                      y: view.bounds.size.height - 70,
+                                                      width: 100, height: 50))
+    view.addSubview(dayNightSwitch)
+    dayNightSwitch.on = true
+
+    theme(MyTheme.self) { controller, theme in
+      UIView.transition(with: controller.imageView,
+                        duration: 0.25,
+                        options: .transitionCrossDissolve,
+                        animations: {
+        controller.imageView.image = theme.image
+      }, completion: nil)
+
+      controller.cityLabel.textColor = theme.textColor
+      controller.timeLabel.textColor = theme.textColor
+    }
+
+    dayNightSwitch.changeAction = { on in
+      Manager.theme = on ? ThemeList.day : ThemeList.night
+    }
   }
 
   override func didReceiveMemoryWarning() {
